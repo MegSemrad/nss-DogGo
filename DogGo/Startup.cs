54db7880f027d655bb2a8a3f82e7f71da676b811
Meghan Semrad <mlsemrad753@gmail.com>
2021-03-26 10:02:09 -0500

    owners show in browser

--- DogGo/Startup.cs
+++ DogGo/Startup.cs
@@ -54,7 +54,7 @@ namespace DogGo
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+        });
         }
     }
 }
